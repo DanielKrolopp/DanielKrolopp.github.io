354cd0a01835bb58728965372f8ebf5816e44617
Daniel Krolopp <dkrolopp@purdue.edu>
2019-01-12 17:29:46 -0500

    Shows location correctly

--- js/occupation.js
+++ js/occupation.js
@@ -1,15 +1,35 @@
 'use strict';
 
+const sandiaDescription = "Starting in May 2019, I will be a summer R&D Intern at Sandia National Laboratories."
+const sandiaLocation = "ALBUQUERQUE, NM"
+
 class OccDetails extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      description: null,
+      occupationLocation: null
+    }
   }
 
   render() {
+    switch(this.props.occupation) {
+      case 'SANDIA NATIONAL LABS':
+        this.state.description = sandiaDescription;
+        this.state.occupationLocation = sandiaLocation;
+        break;
+      default:
+        this.state.description = "Excepteur sint occaecat cupidatat non but see an issue with steps. as per your fiddle, there are definitely three stages to the animation, but it's just getting darker and darker. instead, it should be flashing solid red, green, blue without fading or losing brightness. if i comment out the timing function, it does better at simply flashing each color, but";
+        this.state.occupationLocation = "UNKNOWN, USA"
+        break;
+    }
     return (
       <div className="occupation-section">
-        <h2>{ this.props.organization }</h2>
-        <p>Excepteur sint occaecat cupidatat non but see an issue with steps. as per your fiddle, there are definitely three stages to the animation, but it's just getting darker and darker. instead, it should be flashing solid red, green, blue without fading or losing brightness. if i comment out the timing function, it does better at simply flashing each color, but</p>
+        <h2>{ this.props.occupation }</h2>
+        <span className="inline-span">
+          <strong className="occupation-location">{ this.state.occupationLocation } - </strong>
+          <p> { this.state.description } </p>
+        </span>
       </div>
     );
   }
@@ -23,9 +43,9 @@ class OccBox extends React.Component {
   render() {
     return (
       <div className="occupation-image">
-        <img src={ this.props.imagePath }  alt={ this.props.displayText } className="image"></img>
-        <a className="btn shadow-none rounded-0 occupation-image-overlay" onClick={ () => this.props.onClick(this.props.displayText) }>
-          <span className="occupation-text">{ this.props.displayText }</span>
+        <img src={ this.props.imagePath }  alt={ this.props.occupation } className="image"></img>
+        <a className="btn shadow-none rounded-0 occupation-image-overlay" onClick={ () => this.props.onClick(this.props.occupation) }>
+          <span className="occupation-text">{ this.props.occupation }</span>
         </a>
       </div>
     );
@@ -36,26 +56,26 @@ class MyWorkArea extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showDetails: false
+      occupation: null
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(organization) {
-    this.setState({ showDetails: organization });
+  handleClick(occupation) {
+    this.setState({ occupation: occupation });
   }
 
   render() {
     return (
       <div className="occupation-container">
-        <OccBox displayText='SANDIA NATIONAL LABS' imagePath='images/sandia.jpg' onClick={ this.handleClick } />
-        <OccBox displayText='GRADUATE RESEARCH ASSISTANT' imagePath='images/network.jpg' onClick={ this.handleClick } />
-        <OccBox displayText='M.S. COMPUTER SCIENCE' imagePath='images/fallpurdue.jpg' onClick={ this.handleClick } />
-        <OccBox displayText='UNDERGRAD TEACHING ASSISTANT' imagePath='images/circuitry.jpg' onClick={ this.handleClick } />
-        <OccBox displayText='MOBI WIRELESS MANAGEMENT' imagePath='images/mobi.png' onClick={ this.handleClick } />
-        <OccBox displayText='B.S. COMPUTER SCIENCE' imagePath='images/purduebelltower.jpg' onClick={ this.handleClick } />
-        <OccBox displayText='EARLIER' imagePath='images/gbs.jpg' onClick={ this.handleClick } />
-        { this.state.showDetails && <OccDetails organization={ this.state.showDetails } /> }
+        <OccBox occupation='SANDIA NATIONAL LABS' imagePath='images/sandia.jpg' onClick={ this.handleClick } />
+        <OccBox occupation='GRADUATE RESEARCH ASSISTANT' imagePath='images/network.jpg' onClick={ this.handleClick } />
+        <OccBox occupation='M.S. COMPUTER SCIENCE' imagePath='images/fallpurdue.jpg' onClick={ this.handleClick } />
+        <OccBox occupation='UNDERGRAD TEACHING ASSISTANT' imagePath='images/circuitry.jpg' onClick={ this.handleClick } />
+        <OccBox occupation='MOBI WIRELESS MANAGEMENT' imagePath='images/mobi.png' onClick={ this.handleClick } />
+        <OccBox occupation='B.S. COMPUTER SCIENCE' imagePath='images/purduebelltower.jpg' onClick={ this.handleClick } />
+        <OccBox occupation='EARLIER' imagePath='images/gbs.jpg' onClick={ this.handleClick } />
+        { this.state.occupation && <OccDetails occupation={ this.state.occupation } /> }
       </div>
     );
   }
